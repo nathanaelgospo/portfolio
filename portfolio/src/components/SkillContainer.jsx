edd80aaa5315906a26b3575c35524f0ebe00accf
nathanaelgospo <nathanaelgospo@gmail.com>
2025-11-05 15:10:37 -0500

    skills page progreess

--- portfolio/src/components/SkillContainer.jsx
+++ portfolio/src/components/SkillContainer.jsx
@@ -4,7 +4,14 @@ export default function SkillContainer({ skill }) {
 
     return (
         <div className={styles.container}>
-
+            <div className={styles.data}>
+                <img
+                    src={skill.image}
+                    alt={`${skill.skill} logo`}
+                    className={styles.image}
+                />
+                <h2>{skill.skill}</h2>
+            </div>
         </div>
     )
 }
--- portfolio/src/pages/Skills.jsx
+++ portfolio/src/pages/Skills.jsx
@@ -1,9 +1,30 @@
 import SkillContainer from "../components/SkillContainer";
 import styles from "./Skills.module.css"
+import javascript from "../assets/javascript.png";
+import java from "../assets/java.png";
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import react from "../assets/react.png";
+import git from "../assets/git.png";
+import typescript from "../assets/typescript.png";
+import cSharp from "../assets/cSharp.png";
+import c from "../assets/c.png";
+import python from "../assets/python.png";
+import ruby from "../assets/ruby.png";
 
 export default function Skills() {
 
-    const javaScriptSkill = { skill: "javascript" };
+    const javaScriptSkill = { skill: "Javascript", image: javascript };
+    const javaSkill = { skill: "Java", image: java };
+    const htmlSkill = { skill: "HTML", image: html };
+    const cssSkill = { skill: "CSS", image: css };
+    const reactSkill = { skill: "React", image: react };
+    const gitSkill = { skill: "Git/Github", image: git };
+    const typeScriptSkill = { skill: "Typescript", image: typescript };
+    const cSharpSkill = { skill: "C#", image: cSharp };
+    const cSkill = { skill: "C", image: c };
+    const pythonSkill = { skill: "Python", image: python };
+    const rubySkill = { skill: "Ruby", image: ruby };
 
     return (
 
@@ -12,6 +33,16 @@ export default function Skills() {
                 <h1> My <span className={styles.highlight}> Skills </span></h1>
                 <div className={styles.grid}>
                     <SkillContainer skill={javaScriptSkill} />
+                    <SkillContainer skill={javaSkill} />
+                    <SkillContainer skill={htmlSkill} />
+                    <SkillContainer skill={cssSkill} />
+                    <SkillContainer skill={reactSkill} />
+                    <SkillContainer skill={gitSkill} />
+                    <SkillContainer skill={typeScriptSkill} />
+                    <SkillContainer skill={cSharpSkill} />
+                    <SkillContainer skill={cSkill} />
+                    <SkillContainer skill={pythonSkill} />
+                    <SkillContainer skill={rubySkill} />
                 </div>
             </div>
         </section>
